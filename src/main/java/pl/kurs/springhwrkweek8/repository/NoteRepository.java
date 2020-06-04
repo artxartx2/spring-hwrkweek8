00237177aa0711da68f074ac63152f576d5d70fc
Artur <artx@post.pl>
2020-06-04 18:36:54 +0200

    Small cleanup ( not needed @Bean and pom dependencies)

--- src/main/java/pl/kurs/springhwrkweek8/repository/NoteRepository.java
+++ src/main/java/pl/kurs/springhwrkweek8/repository/NoteRepository.java
@@ -1,8 +1,9 @@
 package pl.kurs.springhwrkweek8.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import pl.kurs.springhwrkweek8.model.Note;
-
+@Repository
 public interface NoteRepository  extends JpaRepository<Note,Long> {
 
 }
